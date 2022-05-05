5994b3b32807e792601e6b4144dcb7cd3de4fbd8
Kym Reilly <kymreilly@hotmail.com>
2022-05-05 22:55:10 +0930

    working prototype, readme created, much more functionality still required

--- index.js
+++ index.js
@@ -2,16 +2,71 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// const createReadMe = {};
+const createReadMe = ({ projectName, description, installation, usage }) =>
+  `**${projectName}
+3
+${description}
+1
+${installation}
+2
+${usage}
+
+`;
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is your Project Name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a brief description of the project...",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "?",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "contributors",
+    //     message: "?",
+    //   },
+    //   {
+    //     type: "input",
+    //     name: "tests",
+    //     message: "?",
+  },
+];
+// inquirer.prompt(questions).then((data) => {}
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+  const readMeInfo = createReadMe(data);
+  fs.writeFile(fileName, readMeInfo, (err) =>
+    err ? console.log(err) : console.log("Successfully created README.md!")
+  );
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt([...questions]).then((answers) => {
+    writeToFile("./readme-factory/README.md", answers);
+  });
+}
 
 // Function call to initialize app
 init();
+
+// TOO TIRED FOR MORE
+// working prototype
+//add more homework criteria
+//try not to break
+//badges from shield.io for license apache v2 gnu v3 MIT
+//
